7f17ae39ded0dca48ddbc35c921e806e5c60df9e
mgrimsle <86434961+mgrimsle@users.noreply.github.com>
2021-07-15 14:38:24 -0400

    Drives License Set up BL

---
+++ DMV/Models/DriversLicenseMapper.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using DMV.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DMV.Models
+{
+    public class DriversLicenseMapper : Profile
+    {
+        public DriversLicenseMapper()
+        {
+            CreateMap<DriversLicenseModel, DriversLicense>().ReverseMap();
+
+        }
+
+    }
+}
