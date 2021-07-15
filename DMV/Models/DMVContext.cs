229b253e6df20933c9b18628cb6255d0ed70aef7
Zack Griffin <zack.griffin@capgemini.com>
2021-07-15 10:03:00 -0400

    added dbcontext and driver model

---
+++ DMV/Models/DMVContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace DMV.Models
+{
+    public class DMVContext : DbContext
+    {
+        public DbSet<DriverDataModel> Driver { get; set; }
+    }
+}
